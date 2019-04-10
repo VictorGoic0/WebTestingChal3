0be503b4f6b2c9de9f09a45fb1b469dd7611357f
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-10 18:38:12 -0400

    Added some more tests for lock/unlock, open/close functionality

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,7 +7,35 @@ describe("<Dashboard />", () => {
   it("renders successfully", () => {
     render(<Dashboard />);
   });
-  it("locks successfully, and only when gate is closed", () => {
+  it("gate opens and closes successfully when unlocked", () => {
+    const { getByText } = render(<Dashboard />);
+    const gate = getByText(/Close/);
+    fireEvent.click(gate);
+    fireEvent.click(gate);
+    fireEvent.click(gate);
+    fireEvent.click(gate);
+    getByText(/Open/);
+  });
+  it("gate locks successfully", () => {
+    const { getByText } = render(<Dashboard />);
+    const lock = getByText(/Lock/);
+    const gate = getByText(/Close/);
+    fireEvent.click(gate);
+    fireEvent.click(lock);
+    getByText(/Locked/);
+    getByText(/Closed/);
+  });
+  it("gate locks and unlocks successfully", () => {
+    const { getByText } = render(<Dashboard />);
+    const lock = getByText(/Lock/);
+    const gate = getByText(/Close/);
+    fireEvent.click(gate);
+    fireEvent.click(lock);
+    fireEvent.click(lock);
+    getByText(/Unlocked/);
+    getByText(/Closed/);
+  });
+  it("locks only when gate is closed, opens only when unlocked", () => {
     const { getByText } = render(<Dashboard />);
     const lock = getByText(/Lock/);
     const gate = getByText(/Close/);
